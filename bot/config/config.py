25e755641542a4f9715dc3e25462af829e7a724f
TrueDNA <fun2ind@gmail.com>
2024-10-23 18:23:22 +0530

    Add files via upload

---
+++ bot/config/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
+
+    API_ID: int
+    API_HASH: str
+
+    ADVANCED_ANTI_DETECTION: bool = False
+
+    REF_LINK: str = "https://t.me/pocketfi_bot/Mining?startapp=6142904262"
+
+    USE_PROXY_FROM_FILE: bool = False
+
+
+settings = Settings()
+
+
